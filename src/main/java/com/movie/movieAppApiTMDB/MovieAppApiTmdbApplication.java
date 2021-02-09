238c72c983cc04439dac5684b6be88e53645568f
poczta1100 <poczta1100@outlook.com>
2021-02-09 12:44:42 +0100

    Base

---
+++ src/main/java/com/movie/movieAppApiTMDB/MovieAppApiTmdbApplication.java
@@ -0,0 +1,13 @@
+package com.movie.movieAppApiTMDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAppApiTmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAppApiTmdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie/movieAppApiTMDB/MovieAppApiTmdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.movieAppApiTMDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAppApiTmdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
